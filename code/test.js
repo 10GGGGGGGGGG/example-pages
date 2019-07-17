e592908328a45dff9e69b10b68b8fde6d7b1b2d2
10GGGGGGGGGG <47125167+10GGGGGGGGGG@users.noreply.github.com>
2019-07-17 23:20:04 +0200

    Add files via upload

---
+++ code/test.js
@@ -0,0 +1,18 @@
+
+var x=0;
+var y=0;
+
+function setup(){
+createCanvas(windowWidth, windowHeight);
+}
+function draw(){
+background(200);
+ellipse(x,y,20,20);
+move();
+}
+function move(){
+  x+=2;
+  y+=2;
+}
+function checkSides(){
+}
