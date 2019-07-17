1f834da339e19d8ea8a327bca11551f915610d7c
10GGGGGGGGGG <47125167+10GGGGGGGGGG@users.noreply.github.com>
2019-07-17 23:23:14 +0200

    Update test.js

--- code/test.js
+++ code/test.js
@@ -1,18 +1,23 @@
-
-var x=0;
-var y=0;
+var pos{
+x=0;
+y=0;
+}
 
 function setup(){
 createCanvas(windowWidth, windowHeight);
+  pos.x=random(width);
+  pos.y=random(width);
 }
 function draw(){
 background(200);
-ellipse(x,y,20,20);
+ellipse(pos.x,pos.y,20,20);
 move();
+checkSides();
 }
 function move(){
-  x+=2;
-  y+=2;
+  pos.x+=2;
+  pos.y+=2;
 }
 function checkSides(){
+  
 }
