861cc87a79bf01586c74fe5694c522b7c19a4c1b
10GGGGGGGGGG <47125167+10GGGGGGGGGG@users.noreply.github.com>
2019-07-18 01:20:48 +0200

    Update test.js

--- code/test.js
+++ code/test.js
@@ -4,19 +4,44 @@ var vx=3;
 var vy=3;
 var ancho=340;
 var alto=173;
-var img;
+var img1;
+var img2;
+var img3;
+var img4;
+var img5;
+var img6;
+var img7;
+var img8;
+var currentImg;
+var images=[];
 
 function preload() {
-  img = loadImage('code/resources/DVD.png');
+  img1 = loadImage('code/resources/green.png');
+  img2 = loadImage('code/resources/purple.png');
+  img3 = loadImage('code/resources/red.png');
+  img4 = loadImage('code/resources/blue.png');
+  img5 = loadImage('code/resources/yellow.png');
+  img6 = loadImage('code/resources/pink.png');
+  img7 = loadImage('code/resources/orange.png');
+  img8 = loadImage('code/resources/white.png');
+  images.push(img1);
+  images.push(img2);
+  images.push(img3);
+  images.push(img4);
+  images.push(img5);
+  images.push(img6);
+  images.push(img7);
+  images.push(img8);
 }
 function setup(){
 createCanvas(windowWidth, windowHeight);
-  x=random(width-ancho);
-  y=random(height-alto);
+x=random(width-ancho);
+y=random(height-alto);
+currentImg=random(images);
 }
 function draw(){
 background(200);
-image(img,x,y,ancho,alto);
+image(currentImg,x,y,ancho,alto);
 move();
 checkSides();
 }
@@ -26,11 +51,11 @@ function move(){
 }
 function checkSides(){
   if(x>=width-ancho||x<=0){
-  tint(random(255),random(255),random(255));
+  currentImg=random(images);
   vx=-vx;
   }
   if(y>=height-alto||y<=0){
-  tint(random(255),random(255),random(255));
+  currentImg=random(images);
   vy=-vy;
   }
 }
