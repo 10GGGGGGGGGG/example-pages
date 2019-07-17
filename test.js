3b5946b5db3f1620889d35c2d0e2d4b2a70929d6
10GGGGGGGGGG <47125167+10GGGGGGGGGG@users.noreply.github.com>
2019-07-17 22:57:55 +0200

    Update test.js

--- test.js
+++ test.js
@@ -3,17 +3,17 @@ var y=0;
 
 function setup(){
 createCanvas(windowWidth, windowHeight);
-background(200);
 /*x=random(width);
 y=random(height); */
 }
 function draw(){
+background(200);
 ellipse(x,y,20,20);
 move();
 }
 function move(){
-  x+=5;
-  y+=5;
+  x+=2;
+  y+=2;
 }
 function checkSides(){
 }
